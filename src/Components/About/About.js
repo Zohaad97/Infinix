a4a3a1a43bcb1df7c0306f28ca8526720d9f8273
zohaadafzal23 <zohaad.afzal@sadapay.pk>
2024-02-04 19:47:50 +0500

    Style improvements

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -31,7 +31,7 @@ gsap.registerPlugin(ScrollTrigger);
 const About = () => {
 
   return (
-    <Container title={"About"} renderSubtitle={() => <div className="about-desc">
+    <Container hideBackground title={"About"} renderSubtitle={() => <div className="about-desc">
       We are obsessed with making the most of technology,
       putting it out there for people to have fun together in
       the most exciting way possible while playing immersive
@@ -60,7 +60,7 @@ const About = () => {
         </div>
       </div>}
 
-      renderExtra={() => <> <section className="section is-full logo-changer is-logos">
+      renderExtra={() => <> <section style={{ zIndex: 2 }} className="container-medium">
         <div className="approach-container space-top-10">
           <div className="approach-left">
             <h2 className="h2 humongus approach-heading">Approach</h2>
@@ -131,18 +131,11 @@ const About = () => {
         </div>
 
         <div
-          style={{ top: "40%", position: 'absolute' }}
+          style={{ top: "10%", position: 'absolute' }}
           data-w-id="83518ac2-f205-f7c2-7dd0-8484af416ef0"
           className="gradient__image-wrapper"
         >
-          <img
-            src={background}
-            loading="eager"
-            sizes="(max-width: 479px) 100vw, 97vw"
-            srcSet={`${background_500} 500w, ${background_800} 800w, ${background_1080} 1080w, ${background} 1200w`}
-            alt=""
-            className="gradient__image is-home-hero-right"
-          />
+
           <img
             src={background}
             loading="eager"
@@ -156,171 +149,5 @@ const About = () => {
       }
     />
   )
-  return (
-    <div className="body">
-      <div className="globals">
-        <Navigation />
-      </div>
-      <main className="page__wrap">
-        <div
-          id="hero"
-          tr-scrollflip-element="component"
-          className="teleport-component"
-        >
-          <section
-            tr-scrollflip-scrubstart="top top"
-            className="section-about-page"
-          >
-            <div className="page-spacing__medium test">
-              <div className="container-large about-content">
-                <div className="spacing-bottom-04-px is-track-record bg--2 radius-24">
-                  <div className="about-container container-small is-track-record">
-                    <div className="record-div bg--4">
-                      <div className="record__heading-div">
-                        <h2 className="p-leading opacity-06">About</h2>
-                      </div>
-                    </div>
-
-                    <h1 className="about-heading">
-                      Our Winning <span className="blue-text">formula</span> is
-                    </h1>
-
-                    <div className="about-desc">
-                      We are obsessed with making the most of technology,
-                      putting it out there for people to have fun together in
-                      the most exciting way possible while playing immersive
-                      technologies.
-                    </div>
-                    <div className="about-image">
-
-                      <img src={group_12} alt="" />
-                      <div className="about-image-overlay">
-                        <span>
-                          <h5 className="about-value-text"> ENGINEERING</h5>
-
-                        </span>
-                        <span>
-                          <h5 className="about-value-text"> COLLABORATION</h5>
-
-                        </span>
-                        <span>
-                          <h5 className="about-value-text"> EXECUTION</h5>
-
-                        </span>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-
-        <section className="section is-full logo-changer is-logos">
-          <div className="approach-container space-top-10">
-            <div className="approach-left">
-              <h2 className="h2 humongus approach-heading">Approach</h2>
-              <div className="approach-left-content">
-                <img src={frame_1} alt="" />
-                <h3 className="approach-title space-top-10">Our Commitment</h3>
-                <div class="text-balance approach-desc space-top-10">
-                  Dedication is at the core of our team's ethos. We strive to be
-                  trendsetters in the event technology landscape, consistently
-                  delivering moments that leave a lasting impression on your
-                  audience.
-                </div>
-                <div
-                  data-w-id="581c9748-1d05-0e08-4e64-c932eb159c10"
-                  className="separator bg--1 opacity-02 seperator-about"
-                ></div>
-              </div>
-              <div className="approach-left-content">
-                <img src={Graphic_Elements} alt="" />
-                <h3 className="approach-title space-top-10">
-                  What makes us Different
-                </h3>
-                <div class="text-balance approach-desc space-top-10">
-                  Offering flexible rental plans to meet specific requirements,
-                  ensuring a customized AV solution for every event. Quality
-                  Assurance and Vendor Connections: Ensuring reliability and
-                  efficiency by partnering with top-quality vendors, we provide
-                  excellence beyond just equipment, contributing to the success
-                  of your event.
-                </div>
-                <div
-                  data-w-id="581c9748-1d05-0e08-4e64-c932eb159c10"
-                  className="separator bg--1 opacity-02 space-top-10 seperator-about"
-                ></div>
-              </div>
-              <div className="approach-left-content">
-                <img src={Graphic_Elements_1} alt="" />
-                <h3 className="approach-title space-top-10">
-                  Reliable Technology
-                </h3>
-                <div class="text-balance approach-desc space-top-10">
-                  For us, technology is not just a tool; it's a reliable partner
-                  for creating exceptional event experiences. Our team ensures
-                  every technical element adds value to your event, delivering a
-                  seamless experience.
-                </div>
-              </div>
-            </div>
-
-            <div className="approach-right">
-              <img src={Circle} alt="" />
-            </div>
-          </div>
-        </section>
-
-        <div
-          style={{ top: "-20%", position: 'absolute' }}
-          data-w-id="83518ac2-f205-f7c2-7dd0-8484af416ef0"
-          className="gradient__image-wrapper"
-        >
-          <img
-            src={background}
-            loading="eager"
-            sizes="(max-width: 479px) 100vw, 97vw"
-            srcSet={`${background_500} 500w, ${background_800} 800w, ${background_1080} 1080w, ${background} 1200w`}
-            alt=""
-            className="gradient__image is-home-hero-right"
-          />
-        </div>
-
-        <div
-          style={{ top: "40%", position: 'absolute' }}
-          data-w-id="83518ac2-f205-f7c2-7dd0-8484af416ef0"
-          className="gradient__image-wrapper"
-        >
-          <img
-            src={background}
-            loading="eager"
-            sizes="(max-width: 479px) 100vw, 97vw"
-            srcSet={`${background_500} 500w, ${background_800} 800w, ${background_1080} 1080w, ${background} 1200w`}
-            alt=""
-            className="gradient__image is-home-hero-right"
-          />
-          <img
-            src={background}
-            loading="eager"
-            sizes="100vw"
-            srcSet={`${background_500} 500w, ${background_800} 800w, ${background_1080} 1080w, ${background} 1200w`}
-            alt=""
-            className="gradient__image is-home-hero"
-          />
-        </div>
-        <section className="section is-full logo-changer in-home is-8">
-          <div className="related_section radius-36">
-            <div className="page-spacing__medium is-related-articles">
-              <NewsLetter />
-
-              <Footer />
-            </div>
-          </div>
-        </section>
-      </main>
-    </div>
-  );
 };
 export default About;
