403c2cfd56b751a853eb9baf424fc5882a4f6e32
zohaadafzal23 <zohaad.afzal@sadapay.pk>
2024-02-07 23:00:00 +0500

    Project images

--- src/Components/ImageSlider/index.js
+++ src/Components/ImageSlider/index.js
@@ -1,44 +1,36 @@
 import React, { useRef } from 'react';
 import './slider.css'; // You can customize the styles in this CSS file
 
+function sliceArrayIntoPairs(array) {
+  const result = [];
+  for (let i = 0; i < array.length; i += 2) {
+    result.push(array.slice(i, i + 2));
+  }
+  return result;
+}
+
 const ImageSlider = ({ images }) => {
-  const sliderRef = useRef(null);
 
-  const scrollLeft = () => {
-    if (sliderRef.current) {
-      sliderRef.current.scrollBy({
-        left: -150, // Adjust the scroll distance as needed
-        behavior: 'smooth',
-      });
-    }
-  };
-
-  const scrollRight = () => {
-    console.log(sliderRef.current.scrollTo)
-    if (sliderRef.current) {
-      sliderRef.current.scrollTo({
-        left: sliderRef.current.scrollLeft - 150, // Adjust the scroll distance as needed
-        behavior: 'smooth',
-      });
-    }
-  };
-
+  const slicedImages = sliceArrayIntoPairs(images);
+  console.log({ slicedImages })
   return (
-    <div className="image-slider-container">
-      <div className="image-slider" ref={sliderRef}>
-        {images.map((image, index) => (
-          <div key={index} className="image-slide">
-            <img src={image} alt={`Slide ${index}`} />
-          </div>
-        ))}
-      </div>
+    <ul class="project-images-list">
+
+      {slicedImages.map((images, index) => (
+        <li key={index} className="project-images-list-item">
+          <img src={images[0]} alt={`Slide ${index}`} />
+          {images[1] && <img src={images[1]} alt={`Slide ${index}`} />}
+
+        </li>
+      ))}
+
       {/* <button className="scroll-button left" onClick={scrollLeft}>
         &lt;
       </button>
       <button className="scroll-button right" onClick={scrollRight}>
         &gt;
       </button> */}
-    </div>
+    </ul>
   );
 };
 
